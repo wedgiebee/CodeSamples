3cbb860a96fb7369e0c7dcb5d38697c6fa813726
Sean W <we.sean94@gmail.com>
2023-05-02 10:09:45 -0700

    Added code samples from the game Welcome To The Underwhere

---
+++ WelcomeToTheUnderwhere/Environment/Ocean.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// A simple code-only animation to make a transparent cube representing the ocean bob slowly up and down
+public class Ocean : MonoBehaviour
+{
+	[Header("Settings")]
+	public float amplitude;
+	public float period;
+
+	protected float _startY;
+
+	void Start()
+	{
+		_startY = transform.position.y;
+	}
+
+	void Update ()
+	{
+		float sin = Mathf.Sin(Time.time / period);
+		float adjustedSin = (sin + 1) / 2;
+		float yValue = adjustedSin * amplitude;
+		Vector3 pos = transform.position;
+		pos.y = _startY + yValue;
+		transform.position = pos;
+	}
+}
---
+++ WelcomeToTheUnderwhere/Kickables/Kickable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Base class for objects that the player can "kick" (aka interact with and apply some force to)
+// simply by walking into them.
+public class Kickable : MonoBehaviour
+{
+	[Header("References")]
+	public Rigidbody rigid;
+
+	[Header("Settings")]
+	public float horizontalMultiplier = 2f;
+	public float verticalMultiplier = 0f;
+
+	public virtual void Kick(Vector3 kickDir)
+	{
+		rigid.isKinematic = false;
+		rigid.useGravity = true;
+		rigid.velocity = new Vector3(kickDir.x * horizontalMultiplier, kickDir.y * verticalMultiplier, kickDir.z * horizontalMultiplier);
+	}
+}
